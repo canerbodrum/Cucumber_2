bd3c2820caa76a9cd82a3ba50f3ccb318d502f09
Ahmet CANER <ahmet.caner1.ac@gmail.com>
2023-11-22 21:41:40 +0300

    TestNg.xml smoke test jenkins

--- src/test/java/Runners/_05_TestRunnerRegression.java
+++ src/test/java/Runners/_05_TestRunnerRegression.java
@@ -4,9 +4,10 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-        tags = "@Regression or @SmokeTest",
+        tags = "@Regression or @Smoke",
         features = {"src/test/java/FeatureFiles"},
-        glue = {"StepDefinitions"}
+        glue = {"StepDefinitions"},
+        plugin ={"json:target/cucumber/cucumber.json"}
 )
 public class _05_TestRunnerRegression extends AbstractTestNGCucumberTests {
 }
